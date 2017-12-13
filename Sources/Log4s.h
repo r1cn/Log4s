dc8ef132c20cc199188f3d641fed125ad81f997a
wzxjiang <wzxjiang@foxmail.com>
2017-12-13 18:06:49 +0800

    Initializate

---
+++ Sources/Log4s.h
@@ -0,0 +1,19 @@
+//
+//  Log4s.h
+//  Log4s
+//
+//  Created by wzxjiang on 13/12/2017.
+//  Copyright © 2017 wzxjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Log4s.
+FOUNDATION_EXPORT double Log4sVersionNumber;
+
+//! Project version string for Log4s.
+FOUNDATION_EXPORT const unsigned char Log4sVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Log4s/PublicHeader.h>
+
+
